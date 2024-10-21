cb06232f413c87da8cae22f0502d74c6b8da471c
Duc Tu <tund@sonix.com.vn>
2024-10-21 22:35:30 +0700

    1

--- myapp/services.py
+++ myapp/services.py
@@ -6,10 +6,18 @@ from django.core.exceptions import ValidationError
 from .models import UserTwoFactorAuthData
 
 
-def user_two_factor_auth_data_create(user) :
-    if hasattr(user, "two_factor_auth_data"):
-        raise ValidationError("Can not have more than one 2FA related data.")
+def user_two_factor_auth_data_create(user):
+    # Check if user already has 2FA data
+    if UserTwoFactorAuthData.objects.filter(user=user).exists():
+        # raise ValidationError("User already has 2FA data.")
+        return UserTwoFactorAuthData.objects.filter(user=user).last()
 
-    two_factor_auth_data = UserTwoFactorAuthData.objects.create(user=user, otp_secret=pyotp.random_base32())
+    # Create a new 2FA data object if it doesn't exist
+    otp_secret = pyotp.random_base32()  # Generate a random OTP secret
+    two_factor_auth_data, created = UserTwoFactorAuthData.objects.get_or_create(
+        user=user,
+        defaults={'otp_secret': otp_secret}
+    )
 
-    return two_factor_auth_data
+    if not created:
+        raise ValidationError("Failed to create 2FA data. The user may already have it.")
--- myapp/views2.py
+++ myapp/views2.py
@@ -10,7 +10,22 @@ from .services import user_two_factor_auth_data_create
 class AdminSetupTwoFactorAuthView(TemplateView):
     template_name = "admin_2fa/setup_2fa.html"
 
-    def post(self, request):
+    # def post(self, request):
+    #     context = {}
+    #     user = request.user
+
+    #     try:
+    #         two_factor_auth_data = user_two_factor_auth_data_create(user=user)
+    #         otp_secret = two_factor_auth_data.otp_secret
+
+    #         context["otp_secret"] = otp_secret
+    #         context["qr_code"] = two_factor_auth_data.generate_qr_code(name=user.email)
+    #     except ValidationError as exc:
+    #         context["form_errors"] = exc.messages
+
+    #     return self.render_to_response(context)
+
+    def get(self, request):
         context = {}
         user = request.user
 
@@ -19,46 +34,46 @@ class AdminSetupTwoFactorAuthView(TemplateView):
             otp_secret = two_factor_auth_data.otp_secret
 
             context["otp_secret"] = otp_secret
-            context["qr_code"] = two_factor_auth_data.generate_qr_code(name=user.email)
+            context["qr_code"] = two_factor_auth_data.generate_qr_code(name=user.username)
         except ValidationError as exc:
             context["form_errors"] = exc.messages
 
         return self.render_to_response(context)
 
 
-class AdminConfirmTwoFactorAuthView(FormView):
-    template_name = "admin_2fa/confirm_2fa.html"
-    success_url = reverse_lazy("admin:index")
+# class AdminConfirmTwoFactorAuthView(FormView):
+#     template_name = "admin_2fa/confirm_2fa.html"
+#     success_url = reverse_lazy("admin:index")
 
-    class Form(forms.Form):
-        otp = forms.CharField(required=True)
+#     class Form(forms.Form):
+#         otp = forms.CharField(required=True)
 
-        def clean_otp(self):
-            self.two_factor_auth_data = UserTwoFactorAuthData.objects.filter(user=self.user).first()
+#         def clean_otp(self):
+#             self.two_factor_auth_data = UserTwoFactorAuthData.objects.filter(user=self.user).first()
 
-            if self.two_factor_auth_data is None:
-                raise ValidationError("2FA not set up.")
+#             if self.two_factor_auth_data is None:
+#                 raise ValidationError("2FA not set up.")
 
-            otp = self.cleaned_data.get("otp")
+#             otp = self.cleaned_data.get("otp")
 
-            if not self.two_factor_auth_data.validate_otp(otp):
-                raise ValidationError("Invalid 2FA code.")
+#             if not self.two_factor_auth_data.validate_otp(otp):
+#                 raise ValidationError("Invalid 2FA code.")
 
-            return otp
+#             return otp
 
-    def get_form_class(self):
-        return self.Form
+#     def get_form_class(self):
+#         return self.Form
 
-    def get_form(self, *args, **kwargs):
-        form = super().get_form(*args, **kwargs)
+#     def get_form(self, *args, **kwargs):
+#         form = super().get_form(*args, **kwargs)
 
-        form.user = self.request.user
+#         form.user = self.request.user
 
-        return form
+#         return form
 
-    def form_valid(self, form):
-        form.two_factor_auth_data.rotate_session_identifier()
+#     def form_valid(self, form):
+#         form.two_factor_auth_data.rotate_session_identifier()
 
-        self.request.session["2fa_token"] = str(form.two_factor_auth_data.session_identifier)
+#         self.request.session["2fa_token"] = str(form.two_factor_auth_data.session_identifier)
 
-        return super().form_valid(form)
+#         return super().form_valid(form)
