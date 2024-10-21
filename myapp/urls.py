ed06afd45e01c45d2e059a867239d84d6b456e8f
Duc Tu <tund@sonix.com.vn>
2024-10-21 22:09:17 +0700

    cach 1

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,13 @@
 # myapp/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('main/', views.main_view, name='main'),
     path('otp/', views.otp_view, name='otp'),
+    # path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import logout, authenticate
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
+from .utils import send_otp
+import pyotp
+from django.contrib.auth.models import User
 # View for the login page
 def login_view(request):
     print ('**ldfjdlkf')
@@ -10,15 +14,37 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             request.session['username'] = username
+            send_otp(request)
             return redirect('otp')
         else:
             error_message = 'Invalid U and P'
     return render(request, 'myapp/login.html', {'error_message': error_message})
 
 # View for the main page
+@login_required
 def main_view(request):
     return render(request, 'myapp/main.html')
 
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+
 # View for the OTP page
 def otp_view(request):
+    error_message = None
+    if request.method == 'POST':
+        otp = request.POST['otp']
+        username = request.session['username']
+        otp_secret_key = request.session['otp_secret_key']
+        valid_date = request.session['otp_valid_date']
+        if otp_secret_key:
+            totp =  pyotp.TOTP(otp_secret_key, interval=60)
+            if totp.verify(otp):
+                user = get_object_or_404(User, username=username)
+                login(request, user)
+                del request.session['otp_secret_key']
+                del request.session['otp_valid_date']
+                return redirect('main')
     return render(request, 'myapp/otp.html')
